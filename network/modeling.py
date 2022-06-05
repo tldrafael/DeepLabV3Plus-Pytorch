6d26dd61a2aa39f21648e0e53c1a4eb1bcd2224e
Rafael Toledo <rafaeltol@gmail.com>
2022-06-05 01:30:28 -0300

    Add kwargs to deeplabv3p_res101

--- network/modeling.py
+++ network/modeling.py
@@ -179,7 +179,7 @@ def deeplabv3plus_resnet50(num_classes=21, output_stride=8, pretrained_backbone=
     return _load_model('deeplabv3plus', 'resnet50', num_classes, output_stride=output_stride, pretrained_backbone=pretrained_backbone, **kwargs)
 
 
-def deeplabv3plus_resnet101(num_classes=21, output_stride=8, pretrained_backbone=True):
+def deeplabv3plus_resnet101(num_classes=21, output_stride=8, pretrained_backbone=True, **kwargs):
     """Constructs a DeepLabV3+ model with a ResNet-101 backbone.
 
     Args:
